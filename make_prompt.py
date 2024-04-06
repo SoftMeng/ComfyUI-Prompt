3fa7a259c52fb23c8a4cc399badaf8c565e07776
mengxiangyuan <mengxiangyuancc@gamil.com>
2024-04-06 14:48:04 +0800

    # feat: 一个简单的节点，一个随机生产Prompt的节点.

--- make_prompt.py
+++ make_prompt.py
@@ -2,6 +2,7 @@ import queue
 import requests
 
 q = queue.Queue()
+q_style = ''
 
 endpoint = 'https://jmai.art'
 path = '/api/prompt/make'
@@ -14,8 +15,10 @@ class ComfyUI_Prompt:
 
     @classmethod
     def INPUT_TYPES(s):
+        styles = ['随机','女孩','设计','风景','风格','摄影','恐怖','怪物','手办','微观','中国风','剪影','赛博','电影','诡异','3D']
         return {
             "required": {
+                "style": (styles,),
                 "seed": ("STRING", {"multiline": False}),
                 "log_prompt": (["No", "Yes"], {"default": "Yes"}),
             },
@@ -30,14 +33,21 @@ class ComfyUI_Prompt:
     FUNCTION = "random_prompt"
     CATEGORY = "ComfyUI_Mexx"
 
-    def random_prompt(self, seed, log_prompt):
-        if q.empty():
+    def random_prompt(self, style, seed, log_prompt):
+        global q_style
+        if q.empty() or q_style != style:
+            q.queue.clear()
             # Build request
             headers = {'Content-Type': 'application/json'}
-            payload = {'seed': seed}
+            payload = {
+                'seed': seed,
+                'name': style
+            }
 
             # Send request
             r = requests.get(url, params=payload, headers=headers)
+            q_style = style
+            print(f"style: {style}")
             result = r.json()
             q.put(result["data"][0]["prompt"])
             q.put(result["data"][1]["prompt"])
